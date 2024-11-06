40efc168d2cdd91b1e82a4f912ecab6252f67eeb
James Schinwald <james.schinwald@nauticalcommerce.com>
2024-11-06 08:23:17 -0500

    feat: added dictionary tooltip

--- app/components/dictionary.tsx
+++ app/components/dictionary.tsx
@@ -1,9 +1,35 @@
 import { PropsWithChildren } from "react"
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "~/components/primitives/ui/tooltip"
 
-export const Dictionary: React.FC<PropsWithChildren> = ({ children }) => {
+export const Root: React.FC<PropsWithChildren> = ({ children }) => {
   return (
-    <span className='underline decoration-dashed decoration-tertiary italic cursor-help'>
+    <TooltipProvider>
+      <Tooltip delayDuration={0}>
+        {children}
+      </Tooltip>
+    </TooltipProvider>
+  )
+}
+
+export const Word: React.FC<PropsWithChildren> = ({ children }) => {
+  return (
+    <TooltipTrigger>
+      <span className='underline decoration-dashed decoration-tertiary italic cursor-help'>
+        {children}
+      </span>
+    </TooltipTrigger>
+  )
+}
+
+export const Explanation: React.FC<PropsWithChildren> = ({ children }) => {
+  return (
+    <TooltipContent className='bg-background-soft p-4 rounded-md border-none outline max-w-xs'>
       {children}
-    </span>
+    </TooltipContent>
   )
 }
