26b9d3110c355cde72046ee3e73fbdcb619d241d
James Schinwald <james.schinwald@nauticalcommerce.com>
2024-12-22 00:09:59 -0500

    feat: cleaned up cards

--- app/routes/about.learn-more/components/card.tsx
+++ app/routes/about.learn-more/components/card.tsx
@@ -2,8 +2,11 @@ import { PropsWithChildren } from "react"
 
 export const Root: React.FC<PropsWithChildren> = ({ children }) => {
   return (
-    <div className='relative bg-background-overlay text-foreground-overlay p-6 rounded-md flex flex-col w-full h-full gap-4'>
-      {children}
+    <div className=' text-foreground-overlay rounded-md w-full h-full gap-4 grid overflow-clip outline outline-[#fff2]'>
+      <div className='row-span-full col-span-full bg-background-overlay backdrop-blur-lg opacity-40' />
+      <div className='row-span-full col-span-full flex flex-col p-6 relative gap-4'>
+        {children}
+      </div>
     </div>
   )
 }
@@ -23,28 +26,3 @@ export const Content: React.FC<PropsWithChildren> = ({ children }) => {
     </div>
   )
 }
-
-// export const Root: React.FC = () => {
-//   return (
-//     <div className='border border-[#fff3] p-3 rounded-sm flex flex-row items-start gap-2 max-h-[58px] overflow-hidden'>
-//       <div className='flex flex-col gap-4'>
-//         <div className='flex flex-col justify-between h-8 py-[2px]'>
-//           <h6 className='text-md'>University of Guelph</h6>
-//           <p className='font-thin text-sm'>Bachelor of Computing</p>
-//         </div>
-//         <div className='flex flex-col justify-between h-8'>
-//           <h6 className='text-md'>2017 - 2021</h6>
-//           <p className='font-thin text-sm'>Guelph, ON</p>
-//         </div>
-//       </div>
-//     </div>
-//   )
-// }
-//
-// export const Image: React.FC = () => {
-//   return <img className='w-8' src={education.universityOfGuelphLogo.url} alt={education.universityOfGuelphLogo.alt} />
-// }
-//
-// export const Detail: React.FC = () => {
-//
-// }
