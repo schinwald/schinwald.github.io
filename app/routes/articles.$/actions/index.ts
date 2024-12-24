026af06de4baa12b7ef76f0be519e4a2c148b6d2
James Schinwald <james.schinwald@nauticalcommerce.com>
2024-12-24 18:12:21 -0500

    feat: added individual article page

---
+++ app/routes/articles.$/actions/index.ts
@@ -0,0 +1,8 @@
+import { z } from "zod";
+import { actionHandler } from "~/utils/remix/action.server";
+
+const schema = z.object({});
+
+export const action = actionHandler(schema, async () => {
+	// TODO: add some actions
+});
