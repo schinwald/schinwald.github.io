ad619263e5fb6870be4527ca676c8c8d416e457e
James Schinwald <james.schinwald@nauticalcommerce.com>
2024-12-23 20:05:02 -0500

    fix: updated background to have a gradient

--- app/routes/$/page.tsx
+++ app/routes/$/page.tsx
@@ -2,7 +2,7 @@ import { BackgroundGradient } from '~/components/background-gradient';
 
 export default function() {
   return (
-    <main className='w-screen h-screen bg-background text-foreground flex flex-col justify-center items-center'>
+    <main className='w-screen h-screen text-foreground flex flex-col justify-center items-center'>
       <div className='flex flex-col justify-center items-center gap-2'>
         <h2>Page Not Found</h2>
         <p>Are you lost?</p>
