42b11bfb78693d42f130c24dc0ef19f3385b0a2f
James Schinwald <james.schinwald@nauticalcommerce.com>
2025-01-01 19:40:09 -0500

    feat: added checklist support to mdx

--- app/routes/articles.$id/loader/index.ts
+++ app/routes/articles.$id/loader/index.ts
@@ -24,6 +24,108 @@ const globals = {
 };
 
 // Rehype Plugin to Transform Callouts
+// Remove p tag from checklist item
+// Include ul/ol tag
+function rehypeChecklistItem() {
+	return (tree: Root) => {
+		visit(tree, (node, index, parent) => {
+			if (parent === undefined) return;
+			if (index === undefined) return;
+
+			if (node.type === "element" && node.tagName === "li") {
+				const newChildren = [];
+				for (const child of node.children) {
+					// Add raw text nodes
+					// This is used when there is no nested lists
+					if (child.type === "text") {
+						if (child.value === "\n") {
+							continue;
+						}
+
+						// This is checking for the "[x] task" patterns
+						const matching = child.value.match(/^\[(x| )\] (.+)/);
+
+						// Matches the checklist item
+						if (matching) {
+							const [, checkedCharacter, text] = matching;
+							newChildren.push({
+								type: "element",
+								tagName: "p",
+								children: [
+									{
+										type: "text",
+										value: text,
+									},
+								],
+							} satisfies ElementContent);
+							node.tagName = "checklist-item";
+							node.properties = {
+								isChecked: checkedCharacter === "x",
+							};
+							continue;
+						}
+
+						// No match means it's a regular list item
+						newChildren.push({
+							type: "element",
+							tagName: "p",
+							children: [child],
+						} satisfies ElementContent);
+						continue;
+					}
+
+					// Add paragraph elements
+					// This is used when there is a nested list inside the list item
+					if (child.type === "element" && child.tagName === "p") {
+						const newGrandChildren = [];
+						for (const grandChild of child.children) {
+							if (grandChild.type === "text") {
+								if (grandChild.value === "\n") {
+									continue;
+								}
+
+								// This is checking for the "[x] task" patterns
+								const matching = grandChild.value.match(/^\[(x| )\] (.+)/);
+
+								// Matches the checklist item
+								if (matching) {
+									const [, checkedCharacter, text] = matching;
+									newGrandChildren.push({
+										type: "text",
+										value: text,
+									} satisfies ElementContent);
+									node.tagName = "checklist-item";
+									node.properties = {
+										isChecked: checkedCharacter === "x",
+									};
+									continue;
+								}
+
+								newGrandChildren.push(grandChild);
+								continue;
+							}
+
+							// We can leave everything else untouched
+							newGrandChildren.push(grandChild);
+						}
+
+						// Add the processed grand children back to the child
+						child.children = newGrandChildren;
+					}
+
+					// We can leave everything else untouched
+					newChildren.push(child);
+				}
+
+				// Add the processed children back to the node
+				node.children = newChildren;
+				parent.children[index] = node;
+			}
+		});
+	};
+}
+
+// Rehype Plugin to Transform Callouts
 function rehypeCode() {
 	return (tree: Root) => {
 		visit(tree, (node) => {
@@ -139,6 +241,7 @@ export const loader = loaderHandler(async ({ params, json }) => {
 				options.rehypePlugins = [
 					...(options.rehypePlugins || []),
 					rehypeSlug, // Generates `id` attributes for headers
+					rehypeChecklistItem,
 					rehypeCode,
 					rehypeCallouts,
 					extractHeaders, // Extract headers into the `toc` array
