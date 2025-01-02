258bc1330b4786b8db4c0e07b1433b03cf5e9410
James Schinwald <james.schinwald@nauticalcommerce.com>
2025-01-01 21:17:31 -0500

    refactor: cleaned up mdx code

---
+++ app/utils/mdx/rehype-listitems.server.ts
@@ -0,0 +1,102 @@
+import type { Root, ElementContent } from "hast";
+import { visit } from "unist-util-visit";
+
+// Rehype plugin to transform list items
+export function rehypeListItem() {
+	return (tree: Root) => {
+		visit(tree, (node, index, parent) => {
+			if (parent === undefined) return;
+			if (index === undefined) return;
+
+			if (node.type === "element" && node.tagName === "li") {
+				const newChildren = [];
+				for (const child of node.children) {
+					// Add raw text nodes
+					// This is used when there is no nested lists
+					if (child.type === "text") {
+						if (child.value === "\n") {
+							continue;
+						}
+
+						// This is checking for the "[x] task" patterns
+						const matching = child.value.match(/^\[(x| )\] (.+)/);
+
+						// Matches the checklist item
+						if (matching) {
+							const [, checkedCharacter, text] = matching;
+							newChildren.push({
+								type: "element",
+								tagName: "p",
+								children: [
+									{
+										type: "text",
+										value: text,
+									},
+								],
+							} satisfies ElementContent);
+							node.tagName = "checklist-item";
+							node.properties = {
+								isChecked: checkedCharacter === "x",
+							};
+							continue;
+						}
+
+						// No match means it's a regular list item
+						newChildren.push({
+							type: "element",
+							tagName: "p",
+							children: [child],
+						} satisfies ElementContent);
+						continue;
+					}
+
+					// Add paragraph elements
+					// This is used when there is a nested list inside the list item
+					if (child.type === "element" && child.tagName === "p") {
+						const newGrandChildren = [];
+						for (const grandChild of child.children) {
+							if (grandChild.type === "text") {
+								if (grandChild.value === "\n") {
+									continue;
+								}
+
+								// This is checking for the "[x] task" patterns
+								const matching = grandChild.value.match(/^\[(x| )\] (.+)/);
+
+								// Matches the checklist item
+								if (matching) {
+									const [, checkedCharacter, text] = matching;
+									newGrandChildren.push({
+										type: "text",
+										value: text,
+									} satisfies ElementContent);
+									node.tagName = "checklist-item";
+									node.properties = {
+										isChecked: checkedCharacter === "x",
+									};
+									continue;
+								}
+
+								newGrandChildren.push(grandChild);
+								continue;
+							}
+
+							// We can leave everything else untouched
+							newGrandChildren.push(grandChild);
+						}
+
+						// Add the processed grand children back to the child
+						child.children = newGrandChildren;
+					}
+
+					// We can leave everything else untouched
+					newChildren.push(child);
+				}
+
+				// Add the processed children back to the node
+				node.children = newChildren;
+				parent.children[index] = node;
+			}
+		});
+	};
+}
