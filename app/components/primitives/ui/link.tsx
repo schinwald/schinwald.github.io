e1360d691c89b58f4a425b297f4e67ca0546c7da
James Schinwald <james.schinwald@nauticalcommerce.com>
2025-01-05 20:23:01 -0500

    fix: remove rounded from link

--- app/components/primitives/ui/link.tsx
+++ app/components/primitives/ui/link.tsx
@@ -7,14 +7,14 @@ import { useAnimate } from "framer-motion"
 import { useNavigationStore } from "~/hooks/stores/navigation"
 
 const linkVariants = cva(
-  "inline-flex items-center justify-center whitespace-nowrap rounded-md font-display ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50",
+  "inline-flex items-center justify-center whitespace-nowrap font-display ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50",
   {
     variants: {
       variant: {
-        default: "bg-primary text-primary-foreground hover:bg-primary/90 uppercase rounded-md text-md md:text-xl",
-        destructive: "bg-destructive text-destructive-foreground hover:bg-destructive/90",
-        outline: "border border-primary font-bold text-primary p-2",
-        secondary: "bg-secondary text-secondary-foreground hover:bg-secondary/80",
+        default: "bg-primary text-primary-foreground rounded-md hover:bg-primary/90 uppercase rounded-md text-md md:text-xl",
+        destructive: "bg-destructive text-destructive-foreground rounded-md hover:bg-destructive/90",
+        outline: "border border-primary font-bold rounded-md text-primary p-2",
+        secondary: "bg-secondary text-secondary-foreground rounded-md hover:bg-secondary/80",
         ghost: "hover:bg-accent hover:text-accent-foreground",
         link: "text-primary underline-offset-4 hover:underline",
       },
