837481fa836b4295d069a7eaab19ec1d6b681923
James Schinwald <james.schinwald@nauticalcommerce.com>
2025-04-19 13:58:07 -0400

    feat: add overlay provider and integrate with dictionary

--- app/components/dictionary.tsx
+++ app/components/dictionary.tsx
@@ -7,11 +7,15 @@ import {
 	TooltipProvider,
 	TooltipTrigger,
 } from "~/components/primitives/ui/tooltip";
+import { useOverlay } from "~/hooks/overlay";
 
 export const Root: React.FC<PropsWithChildren> = ({ children }) => {
+	const { setIsOpen } = useOverlay();
 	return (
 		<TooltipProvider>
-			<Tooltip delayDuration={0}>{children}</Tooltip>
+			<Tooltip delayDuration={0} onOpenChange={(isOpen) => setIsOpen(isOpen)}>
+				{children}
+			</Tooltip>
 		</TooltipProvider>
 	);
 };
@@ -31,7 +35,7 @@ export const Explanation: React.FC<PropsWithChildren> = ({ children }) => {
 		<TooltipPortal>
 			<TooltipContent
 				sideOffset={5}
-				className="bg-background-soft p-6 rounded-md max-w-xs shadow-tertiary/10 shadow-lg outline outline-1 outline-tertiary"
+				className="z-50 bg-background-overlay p-6 rounded-md max-w-xs shadow-tertiary/10 shadow-lg outline-1 outline-tertiary"
 			>
 				<div className="flex flex-col items-start gap-4 text-white">
 					<p className="inline-flex items-center gap-2 text-tertiary">
