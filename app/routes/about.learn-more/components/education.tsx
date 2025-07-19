13efb15d9772f651040bbe67749361ec046b97ba
James Schinwald <james.schinwald@nauticalcommerce.com>
2025-07-18 23:24:19 -0400

    feat: used cycler

--- app/routes/about.learn-more/components/education.tsx
+++ app/routes/about.learn-more/components/education.tsx
@@ -1,11 +1,4 @@
-import { useState } from "react";
-import { Button } from "~/components/primitives/ui/button";
-import {
-  Carousel,
-  type CarouselApi,
-  CarouselContent,
-  CarouselItem,
-} from "~/components/primitives/ui/carousel";
+import * as Cycler from "./cycler";
 
 type EducationProps = {
   logo: {
@@ -25,45 +18,31 @@ export const Education: React.FC<EducationProps> = ({
   dates,
   location,
 }) => {
-  const [api, setApi] = useState<CarouselApi>();
-
-  // TODO: make this like the experience component
   return (
-    <Carousel setApi={setApi} opts={{ loop: true }} orientation="horizontal">
-      <Button
-        className="transition-all duration-200 outline-1 outline-[#fff2] hover:outline-[#fff7] p-3 rounded-sm flex flex-row items-start gap-2 max-h-[58px] whitespace-normal h-full"
-        variant="ghost"
-        onClick={() => api?.scrollNext()}
-        onKeyUp={(event) => {
-          if (event.key === "Enter") {
-            api?.scrollNext();
-          }
-        }}
-      >
-        <img className="w-8" src={logo.url} alt={logo.alt} />
-        <div className="flex flex-col gap-6">
-          <CarouselContent>
-            <CarouselItem>
-              <div className="flex flex-col justify-between h-8 py-[2px]">
-                <h6 className="text-left text-md font-bold opacity-75">
-                  {provider}
-                </h6>
-                <p className="text-left text-sm font-thin">{certificate}</p>
-              </div>
-            </CarouselItem>
-            <CarouselItem>
-              <div className="flex flex-col justify-between h-8 py-[2px]">
-                <h6 className="text-left text-md font-bold opacity-75">
-                  {location}
-                </h6>
-                <p className="text-left text-sm font-thin">
-                  {dates[0]} - {dates[1]}
-                </p>
-              </div>
-            </CarouselItem>
-          </CarouselContent>
-        </div>
-      </Button>
-    </Carousel>
+    <div className="transition-all duration-200 outline-1 outline-[#fff2] hover:outline-[#fff7] p-3 rounded-sm flex flex-row items-start gap-2 max-h-[58px] overflow-hidden cursor-pointer">
+      <img className="w-8" src={logo.url} alt={logo.alt} />
+      <div className="flex flex-col gap-6">
+        <Cycler.Root>
+          <Cycler.Item>
+            <div className="flex flex-col justify-between h-8 py-[2px]">
+              <h6 className="text-left text-md font-bold opacity-75">
+                {provider}
+              </h6>
+              <p className="text-left text-sm font-thin">{certificate}</p>
+            </div>
+          </Cycler.Item>
+          <Cycler.Item>
+            <div className="flex flex-col justify-between h-8 py-[2px]">
+              <h6 className="text-left text-md font-bold opacity-75">
+                {location}
+              </h6>
+              <p className="text-left text-sm font-thin">
+                {dates[0]} - {dates[1]}
+              </p>
+            </div>
+          </Cycler.Item>
+        </Cycler.Root>
+      </div>
+    </div>
   );
 };
